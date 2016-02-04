691bab0fec675fe5a509006c9353b1e1dae2b3b8
aimeegott <aimeegott@users.noreply.github.com>
2016-02-04 20:55:47 +0000

    Update roxygen header, add comments

--- R/tidyData.R
+++ R/tidyData.R
@@ -1,14 +1,24 @@
-#' Allow users to search a package for a function
+#' Interactively format data
+#' 
+#' By selecting a data set the user can set all options for working with the \code{gather}
+#' function of the \code{tidyr} package. The user is able to visualise the impact of the
+#' options set and on return the shiny gadget will return the code required to recreate. 
 #'
+#' @author Aimee Gott
+#' 
 #' @export
 tidyData <- function() {
 
   # Get all of the objects in the global environment
   objects <- ls(pos = 1)
+  # determine which are data frames
   dataChoices <- objects[sapply(objects, function(x) is.data.frame(get(x)))]
 
+  # define the UI for the gadget
   ui <- miniUI::miniPage(
     miniUI::gadgetTitleBar("Tidy Data"),
+    # select from data sets available, text for the key and value
+    # multi-select for the columns
     miniUI::miniContentPanel(
       shiny::selectInput("data", "Choose data:", choices = dataChoices),
       shiny::fillRow(height = "15%",
@@ -23,6 +33,7 @@ tidyData <- function() {
   server <- function(input, output, session) {
     # Define reactive expressions, outputs, etc.
     data <- shiny::reactive({
+      # get the selected data and update the column options
       data <- get(input$data)
       shiny::updateSelectInput(session, "cols", choices = names(data))
       data
@@ -30,10 +41,10 @@ tidyData <- function() {
 
     output$gatherData <- shiny::renderTable({
       data <- data()
+      # apply gather to the selected columns
       tidyr::gather_(data, key_col = input$key,
                      value_col = input$value,
                      gather_cols = input$cols)
-      # data()
       })
 
     # When the Done button is clicked, return a value
@@ -43,7 +54,7 @@ tidyData <- function() {
                      ",key=", input$key,
                      ",value=", input$value,
                      ",",cols, ")")
-        # return text for function call
+        # return function call
         shiny::stopApp(rstudioapi::insertText(call))
     })
   }
