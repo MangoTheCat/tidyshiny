c1b3d0a0be56fba5cf00395edc3dc3c48b30e191
Aimee Gott <agott@mango-solutions.com>
2016-03-02 23:52:41 +0000

    Add ability to pass a data.frame as an argument to the function

--- R/tidyData.R
+++ R/tidyData.R
@@ -3,19 +3,26 @@
 #' By selecting a data set the user can set all options for working with the \code{gather}
 #' function of the \code{tidyr} package. The user is able to visualise the impact of the
 #' options set and on return the shiny gadget will return the code required to recreate. 
+#' 
+#' @param data (Optional) Data frame to be manipulated. If not provided the data from 
+#' all data.frame objects in the current environment will be available for selection in the gadget.
 #'
 #' @author Aimee Gott
 #' 
 #' @export
-tidyData <- function() {
+tidyData <- function(data) {
 
-  # Get all of the objects in the global environment
-  objects <- ls(pos = 1)
+  if(missing(data)){
+    # Get all of the objects in the global environment
+    objects <- ls(pos = 1)
+    
+    if(length(objects) == 0) stop("No objects found. Please create a data.frame to continue", call. = FALSE)
+    # determine which are data frames
+    dataChoices <- objects[sapply(objects, function(x) is.data.frame(get(x)))]
+  }else {
+    dataChoices <- as.character(match.call())[2]
+  }
   
-  if(length(objects) == 0) stop("No objects found. Please create a data.frame to continue", call. = FALSE)
-  # determine which are data frames
-  dataChoices <- objects[sapply(objects, function(x) is.data.frame(get(x)))]
-
   # define the UI for the gadget
   ui <- miniUI::miniPage(
     miniUI::gadgetTitleBar("Tidy Data"),
